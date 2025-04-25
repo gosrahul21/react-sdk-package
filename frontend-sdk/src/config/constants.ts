16e1e1b68ace150092ea4806abeb154e8e4edf1a
Rahul <rahul.kumar@vegavid.com>
2025-04-25 16:10:44 +0530

    url updated

--- frontend-sdk/src/config/constants.ts
+++ frontend-sdk/src/config/constants.ts
@@ -4,4 +4,4 @@ export const SDK_DESCRIPTION = "Loan Eligibility SDK";
 export const SDK_AUTHOR = "Loan Eligibility SDK";
 export const SDK_LICENSE = "MIT";
 export const SDK_URL = "http://localhost:3000";
-export const SDK_API_URL = "https://ae4e-2409-40d0-103e-ae4e-e906-ab35-8f76-1e1e.ngrok-free.app";
+export const SDK_API_URL = "https://lark.quantmhill.com";
